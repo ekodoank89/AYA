d884588aab8b8ac9d71bcd7ec7f59c57d69d9336
ekodoank89 <ayaekodoank@gmail.com>
2026-09-02 00:17:17 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/aya/doank/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.aya.doank
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val jakarta = LatLng(-6.2088, 106.8456)
+        googleMap.addMarker(MarkerOptions().position(jakarta).title("Jakarta"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jakarta, 12f))
+    }
+}
