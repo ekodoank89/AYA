8c62c021f753f423d22e1369715f0c99a65d9b3e
ekodoank89 <ayaekodoank@gmail.com>
2026-09-03 09:10:35 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aya.doank"
         minSdk = 30
         targetSdk = 36
-        versionCode = 17
-        versionName = "2.2"
+        versionCode = 18
+        versionName = "2.2.1"
         manifestPlaceholders["MAPS_API_KEY"] = System.getenv("MAPS_API_KEY") ?: ""
     }
 
